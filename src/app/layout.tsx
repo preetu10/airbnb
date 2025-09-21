852f6f77e905c32f02d8a72aae8e5bc0180892bf
Mahfuja <bajibaji1004@gmail.com>
2025-09-21 17:12:53 +0600

    always light mode fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body  className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <I18nProvider>
           <Navbar />
